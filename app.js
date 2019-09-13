3a101e08e8942abd898b1ed8bd60f5b1b4ed742a
BillClinton <bclinton@appunto.net>
2019-09-13 17:29:52 -0400

    App setup, add yargs, chalk, and validator

---
+++ app.js
@@ -0,0 +1,45 @@
+const chalk = require('chalk');
+const yargs = require('yargs');
+
+const getNotes = require('./notes.js');
+
+// Customize yargs version
+yargs.version('1.1.0');
+
+// Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  handler: function() {
+    console.log('Adding a new note');
+  }
+});
+
+// Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: function() {
+    console.log('Removing the note');
+  }
+});
+
+// Create list command
+yargs.command({
+  command: 'list',
+  describe: 'List notes',
+  handler: function() {
+    console.log('Listing notes');
+  }
+});
+
+// Create add command
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  handler: function() {
+    console.log('Reading the note');
+  }
+});
+
+console.log(yargs.argv);
