4e63eca46b154a6ff335c54054bfba3b7493ef7f
BillClinton <bclinton@appunto.net>
2019-09-13 18:53:58 -0400

    Add note adding capabilities

--- notes.js
+++ notes.js
@@ -1,5 +1,43 @@
+const fs = require('fs');
+
 const getNotes = function() {
   return 'Your notes';
 };
 
-module.exports = getNotes;
+const addNote = function(title, body) {
+  const notes = loadNotes();
+  const dupes = notes.filter(function(note) {
+    return note.title === title;
+  });
+
+  if (dupes.length === 0) {
+    notes.push({
+      title,
+      body
+    });
+    saveNotes(notes);
+    console.log('New note added.');
+  } else {
+    console.log('Note title already exists.');
+  }
+};
+
+const loadNotes = function() {
+  try {
+    const dataBuffer = fs.readFileSync('notes.json');
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (e) {
+    return [];
+  }
+};
+
+const saveNotes = function(notes) {
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync('notes.json', dataJSON);
+};
+
+module.exports = {
+  getNotes,
+  addNote
+};
